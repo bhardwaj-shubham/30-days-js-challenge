e0bc4ffa10dcf1558fe612c52719be00ef77fc4b
Shubham Bhardwaj <shubhardwaj10@gmail.com>
2024-07-13 16:40:32 +0530

    Day-1 Challenge complete

---
+++ day1.js
@@ -0,0 +1,37 @@
+// task - 1
+var num = 10;
+// console.log(num);
+
+// task - 2
+let name = 'hello';
+// console.log(name);
+
+// task - 3
+const isLive = false;
+// console.log(isLive);
+
+// task - 4
+const day = 1;
+const str = 'First day';
+const isChallengeStart = true;
+const challenge = {
+  name: '30 Days Javascript Challenge',
+  noOfDays: 30,
+};
+const score = [12, 34, 23, 43];
+
+// console.log(typeof day);
+// console.log(typeof str);
+// console.log(typeof isChallengeStart);
+// console.log(typeof challenge);
+// console.log(typeof score);
+
+// task - 5
+let value = 10;
+value = 20;
+console.log(value);
+
+// task - 6
+const constant = 3.142;
+// constant = 3.14;
+// console.log(constant);    // TypeError: Assignment to constant variable.
