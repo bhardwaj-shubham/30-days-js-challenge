421b93070e108a95ac521e8157ef6b7f07a3ecfe
Shubham Bhardwaj <shubhardwaj10@gmail.com>
2024-07-14 10:14:07 +0530

    Day-2 Challenge Complete

---
+++ day2.js
@@ -0,0 +1,62 @@
+// Activity - 1
+// Task - 1
+const addition = 1 + 3;
+// console.log(addition);
+
+// Task - 2
+const substraction = 4 - 2;
+// console.log(substraction);
+
+// Task - 3
+const multiply = 4 * 2;
+// console.log(multiply);
+
+// Task - 4
+const divide = 4 / 2;
+// console.log(divide);
+
+// Task - 5
+const remainder = 13 % 2;
+// console.log(remainder);
+
+// Activity - 2
+// Task - 6
+let result1 = 2;
+result1 += 4;
+// console.log(result1);
+
+// Task - 7
+let result2 = 2;
+result2 -= 4;
+// console.log(result2);
+
+// Activity - 3
+// Task - 8
+const num1 = 5;
+const num2 = 8;
+// console.log(num1 > num2);
+// console.log(num1 < num2);
+
+// Task - 9
+const num3 = 8;
+// console.log(num1 >= num2);
+// console.log(num1 <= num2);
+
+// Task - 10
+// console.log("8" == num3);
+// console.log("8" === num3);
+
+// Activity - 4
+// Task - 11
+// console.log(num1 === num2 && num2 === num3);
+
+// Task - 12
+// console.log(num1 === num2 || num2 === num3);
+
+// Task - 13
+// console.log(!false);
+
+// Activity - 5
+// Task - 14
+const isPositive = 10 > 0 ? "Positive" : "Negative";
+// console.log(isPositive);
