d3be7f8282e5255662095917a7f50c078944db6b
Shubham Bhardwaj <shubhardwaj10@gmail.com>
2024-07-16 10:07:38 +0530

    Day-4 Challenge Completed

---
+++ day4.js
@@ -0,0 +1,71 @@
+// Activity - 1
+// Task - 1
+// for (let i = 0; i < 10; i++) {
+// 	console.log(i+1);
+// }
+
+// Task - 2
+// for (let i = 1; i <= 10; i++) {
+// 	console.log(5 * i);
+// }
+
+// Activity - 2
+// Task - 3
+// let totalSum = 0;
+// let i = 1;
+// while (i <= 10) {
+// 	totalSum += i;
+// 	i += 1;
+// }
+// console.log(totalSum);
+
+// Task - 4
+// let i = 10;
+// while (i > 0) {
+// 	console.log(i);
+// 	i -= 1;
+// }
+
+// Task - 5
+// let i = 1;
+// do {
+// 	console.log(i);
+// 	i += 1;
+// } while (i <= 5);
+
+// Task - 6
+// let i = 1;
+// let factorial = 1;
+// do {
+// 	factorial *= i;
+// 	i += 1;
+// } while (i <= 5);
+// console.log(factorial);
+
+// Activity - 4
+// Task - 7
+// let str = "";
+// for (let i = 0; i < 5; i++) {
+// 	str = "";
+// 	for (let j = 0; j <= i; j++) {
+// 		str += "* ";
+// 	}
+// 	console.log(str);
+// }
+
+// Activity - 5
+// Task - 8
+// for (let i = 1; i <= 10; i++) {
+// 	if (i === 5) {
+// 		continue;
+// 	}
+// 	console.log(i);
+// }
+
+// Task - 9
+for (let i = 1; i <= 10; i++) {
+	if (i === 7) {
+		break;
+	}
+	console.log(i);
+}
