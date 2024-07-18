af62b741335ba658e1c03bdb548240dbcba128a5
Shubham Bhardwaj <shubhardwaj10@gmail.com>
2024-07-18 10:50:24 +0530

    Day-6 Challenge Complete

---
+++ day6.js
@@ -0,0 +1,63 @@
+// Activity - 1
+// Task - 1
+// const arr = [1, 2, 3, 4, 5];
+// console.log(arr);
+
+// Task - 2
+// console.log(arr[0]);
+// console.log(arr[arr.length-1]);
+
+// Activity - 2
+// Task - 3
+// arr.push(6);
+// console.log(arr);
+
+// Task - 4
+// arr.pop();
+// console.log(arr);
+
+// Task - 5
+// arr.shift();
+// console.log(arr);
+
+// Task - 6
+// arr.unshift(0);
+// console.log(arr);
+
+// Activity - 3
+// Task - 7
+// const doubledArr = arr.map(num => num * 2);
+// console.log(doubledArr);
+
+// Task - 8
+// const evenNum = arr.filter(num => num % 2 === 0);
+// console.log(evenNum);
+
+// Task - 9
+// const totalSum = arr.reduce((sum, num) => sum + num, 0);
+// console.log(totalSum);
+
+// Task - 10
+// for (let i = 0; i < arr.length; i++) {
+// 	console.log(arr[i]);
+// }
+
+// for (let num of arr) {
+// 	console.log(num);
+// }
+
+// Task - 11
+// arr.forEach(num => console.log(num));
+
+// Activity - 5
+// Task - 12
+// const matrix = [
+// 		[1, 2, 3],
+// 		[4, 5, 6],
+// 		[7, 8, 9]
+// 	];
+
+// console.log(matrix);
+
+// Task - 13
+// console.log(matrix[0][2])
