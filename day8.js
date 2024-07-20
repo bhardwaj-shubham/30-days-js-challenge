9e4ea00f2fe42e6eb1322e82d672ca445652e1e2
Shubham Bhardwaj <shubhardwaj10@gmail.com>
2024-07-20 12:02:12 +0530

    Day 8 Challenge Complete

---
+++ day8.js
@@ -0,0 +1,76 @@
+// Activity - 1
+// Task - 1
+// const name = "Rohit";
+// const age = 26;
+// console.log(`My name is ${name} and my age is ${age}.`);
+
+// Task - 2
+// const paragraph = `
+// 		This is paragraph.
+// 		Testing something!
+// `;
+
+// console.log(paragraph);
+
+// Activity - 2
+// Task - 3
+// const arr = [1, 2, 3, 4, 5];
+// const [first, second] = arr;
+// console.log(first, second);
+
+// Task - 4
+// const book = {
+// 	title: "The Power of Now",
+// 	author: "Eckhart Tolle",
+// 	year: "1997"
+// };
+
+// const {title, author} = book;
+// console.log(title, author);
+
+// Activity - 3
+// Task - 5
+// const newArr = [...arr, 11, 12, 13];
+// console.log(newArr);
+
+// Task - 6
+// function add(...args) {
+// 	let sum = 0;
+// 	for (let num of args) {
+// 		sum += num;
+// 	}
+// 	return sum;
+// }
+
+// console.log(add(1, 2, 3, 4, 5));
+
+// Activity - 4
+// Task - 7
+// function multiply(num1, num2 = 1) {
+// 	return num1 * num2;
+// }
+
+// console.log(multiply(12, 4));
+// console.log(multiply(10));
+
+// Activity - 5
+// Task - 8
+// const name = "Ramesh";
+// const age = 28;
+// const person = {
+// 	name,
+// 	age,
+// 	info: function () {
+// 		console.log(`Name: ${this.name}, Age: ${this.age}`);
+// 	}
+// };
+
+// person.info();
+
+// Task - 9
+// const person2 = {
+// 	name: name,
+// 	age: age
+// };
+
+// console.log(person2);
